9bc7f763bb79177fc6a6740a36a42b414a047a01
Chris Keller <127535781+chrisdvadashanta@users.noreply.github.com>
2023-09-08 12:17:31 +0200

    Update models.js

--- models.js
+++ models.js
@@ -23,7 +23,7 @@ let userSchema = mongoose.Schema({
     Password: {type: String, required: true},
     Email: {type: String, required: true},
     BirthDate: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 }); 
 
 userSchema.statics.hashPassword = (password) => {
